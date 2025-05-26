f09f008a2e3c40b00ca46a96a873d5bfb5917a41
Keen <zhekeen90@gmail.com>
2025-05-26 16:11:17 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -1,21 +1,37 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SpinKit",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v9), // Ubah versi iOS sesuai kebutuhan Anda
     ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SpinKit",
-            targets: ["SpinKit"]
-        ),
+            targets: ["SpinKit"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SpinKit",
-            path: "SpinKit/Animations"
-        )
+            dependencies: []),
+        .target(
+            name: "RTSpinKitAnimating",
+            dependencies: ["SpinKit"]),
+        .target(
+            name: "RTSpinKitUtils",
+            dependencies: ["SpinKit"]),
+        .target(
+            name: "RTSpinKitView",
+            dependencies: ["SpinKit"]),
+        .testTarget(
+            name: "SpinKitTests",
+            dependencies: ["SpinKit"]),
     ]
 )
