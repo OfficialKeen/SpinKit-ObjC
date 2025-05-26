9ceece3a4b051d6f9f5ffffaf7a34ca32476ce1e
Keen <zhekeen90@gmail.com>
2025-05-26 20:20:19 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -3,26 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "SpinKit",
-    platforms: [
-        .iOS(.v12), // Atur versi minimum iOS yang didukung
-    ],
     products: [
-        // Mendefinisikan produk utama dari package
         .library(
             name: "SpinKit",
             targets: ["SpinKit"]),
     ],
-    dependencies: [
-        // Menambahkan dependensi eksternal jika ada
-    ],
     targets: [
         .target(
             name: "SpinKit",
+            dependencies: [],
             path: "SpinKit",
-            publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath(".")
-            ]
-        )
+            sources: ["SpinKit/Animations", "SpinKit/RTSpinKitAnimating.h", "SpinKit/RTSpinKitUtils.h", "SpinKit/RTSpinKitUtils.m", "SpinKit/RTSpinKitView.h", "SpinKit/RTSpinKitView.m"])
     ]
 )
