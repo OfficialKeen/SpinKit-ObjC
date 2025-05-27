23eb8cc4ac0125f581929bd11092e6406ae40201
Keen <zhekeen90@gmail.com>
2025-05-27 14:53:27 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,3 @@
-/*
 // swift-tools-version:5.3
 import PackageDescription
 
@@ -27,8 +26,7 @@ let package = Package(
                 "RTSpinKitUtils.h",
                 "RTSpinKitUtils.m",
                 "RTSpinKitView.h",
-                "RTSpinKitView.m",
-                "RTSpinKitAnimating.h"
+                "RTSpinKitView.m"
             ],
             publicHeadersPath: "SpinKit"
         ),
@@ -40,33 +38,5 @@ let package = Package(
                 "SpinKitTests"
             ]
         ),
-    ]
-)*/
-
-// swift-tools-version:5.3
-import PackageDescription
-
-let package = Package(
-    name: "SpinKit",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SpinKit",
-            targets: ["SpinKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SpinKit",
-            path: "SpinKit",
-            dependencies: []),
-        .testTarget(
-            name: "SpinKitTests",
-            dependencies: ["SpinKit"]),
     ]
 )
