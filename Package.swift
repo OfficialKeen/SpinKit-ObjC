90bd6a058c624750003a84c4226e27d8493eb994
Keen <zhekeen90@gmail.com>
2025-05-27 14:59:58 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             sources: [
                 "Animations"
             ],
-            publicHeadersPath: "SpinKit"
+            publicHeadersPath: "."
         ),
         .testTarget(
             name: "SpinKitTests",
