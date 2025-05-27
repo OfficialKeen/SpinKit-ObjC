d7ba6368c189a52644417b3294dfe4c201ba05d6
Keen <zhekeen90@gmail.com>
2025-05-27 15:31:14 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             name: "SpinKit",
             path: "SpinKit/Animations",
             sources: [
-                "Animations"
+                "."
             ],
             publicHeadersPath: "SpinKit"
         ),
