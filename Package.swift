f152b8f6c25065113fef32426c74bc707da8e2c7
Keen <zhekeen90@gmail.com>
2025-05-27 15:32:17 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SpinKit",
-            path: "SpinKit/Animations",
+            path: "SpinKit",
             sources: [
-                "."
+                "Animations"
             ],
             publicHeadersPath: "SpinKit"
         ),
