99f9dbee84c1378dc47168e83a1f68edc8aaf27c
Keen <zhekeen90@gmail.com>
2025-06-01 11:00:40 +0700

    Package

--- SpinKit/Animations/RTSpinKitWordPressAnimation.h
+++ SpinKit/Animations/RTSpinKitWordPressAnimation.h
@@ -23,7 +23,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RTSpinKitAnimating.h"
+#import "./RTSpinKitAnimating.h"
 
 @interface RTSpinKitWordPressAnimation : NSObject<RTSpinKitAnimating>
 
