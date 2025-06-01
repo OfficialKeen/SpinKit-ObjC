b948608db963a87830c91a53221f295ed4cf30f0
Keen <zhekeen90@gmail.com>
2025-06-01 15:09:41 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             sources: [
                 "RTSpinKitAnimating.h"
             ],
-            publicHeadersPath: "."
+            publicHeadersPath: ""
         ),
     ]
 )
